fe4d1fa44bacc41d152391334e6116e1b5db134a
LuanCKRG <luancamposck@gmail.com>
2025-06-23 00:30:28 -0300

    feat(utils): add formatCnpj function for CNPJ formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,16 @@ function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
 
-export { cn }
+function formatCnpj(cnpj: string): string {
+	if (!cnpj || typeof cnpj !== "string") return ""
+
+	const cleaned = cnpj.replace(/\D/g, "")
+
+	if (cleaned.length !== 14) {
+		return cnpj
+	}
+
+	return cleaned.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+}
+
+export { cn, formatCnpj }
