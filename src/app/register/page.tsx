d24bffa200f9d3ec008e8a8bfd69dfbb991c2cb8
LuanCKRG <luancamposck@gmail.com>
2025-06-23 11:31:21 -0300

    feat(register): create RegisterPage component with registration form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegistrationForm } from "@/components/forms/register-partner-form"
+
+const RegisterPage = () => (
+	<main className="flex min-h-screen flex-col items-center justify-center p-4">
+		<RegistrationForm />
+	</main>
+)
+
+export default RegisterPage
