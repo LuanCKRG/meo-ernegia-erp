c7b32b81d73c24634739b4db2816319e140237f7
LuanCKRG <luancamposck@gmail.com>
2025-06-23 00:39:24 -0300

    feat(home): update Home component to fetch and display partners with a registration link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import { RegistrationForm } from "@/components/forms/register-partner-form"
+import { PlusCircle } from "lucide-react"
+import Link from "next/link"
+import { getAllPartners } from "@/actions/partners/get-all-partners"
+import { PartnersTable } from "@/components/partners-table"
+import { Button } from "@/components/ui/button"
 
-const Home = () => {
+const Home = async () => {
+	const partners = await getAllPartners()
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-4">
-			<RegistrationForm />
+		<main className="container mx-auto flex min-h-screen flex-col gap-8 p-4 md:p-8">
+			<div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+				<div className="text-center md:text-left">
+					<h1 className="text-3xl font-bold tracking-tight text-foreground">Parceiros</h1>
+					<p className="text-muted-foreground">Gerencie os parceiros cadastrados no sistema.</p>
+				</div>
+				<Link href="/register" passHref className="w-full md:w-auto">
+					<Button className="w-full md:w-auto">
+						<PlusCircle />
+						Novo Cadastro
+					</Button>
+				</Link>
+			</div>
+			<PartnersTable partners={partners} />
 		</main>
 	)
 }
