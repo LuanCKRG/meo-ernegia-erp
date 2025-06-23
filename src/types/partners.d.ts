4d7bc5d324174a7ae6fceddf70b31d18d944c6fd
LuanCKRG <luancamposck@gmail.com>
2025-06-23 00:31:50 -0300

    feat(types): add Partner type definition for partner entities

---
+++ src/types/partners.d.ts
@@ -0,0 +1,18 @@
+export type Partner = {
+	kdi: number
+	cnpj: string
+	legal_business_name: string
+	contact_name: string
+	contact_mobile: string
+	contact_email: string
+	cep: string
+	street: string
+	number: string
+	complement: string | null
+	neighborhood: string
+	city: string
+	state: string
+	status: string
+	active: boolean
+	created_at: string
+}
