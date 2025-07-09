d5eeea7290ea7c32f83cc5ae061ea7c322883572
amandsalea <amandsalea@gmail.com>
2025-07-09 05:40:49 +0000

    feat(utils): add maskCpf to utils functions

--- src/lib/utils/masks.ts
+++ src/lib/utils/masks.ts
@@ -26,4 +26,13 @@ function maskCep(value: string) {
 		.replace(/(-\d{3})\d+?$/, "$1")
 }
 
-export { maskCnpj, maskCelular, maskCep }
+function maskCpf(value: string) {
+	return value
+		.replace(/\D/g, "")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1.$2")
+		.replace(/(\d{3})(\d)/, "$1-$2")
+		.replace(/(-\d{2})\d+?$/, "$1")
+}
+
+export { maskCnpj, maskCelular, maskCep, maskCpf }
